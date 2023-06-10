57b65ac7e7e1f1772d11701b93051b81503589c5
Kacper Jarych <jarychkacper02@gmail.com>
2023-06-10 08:34:12 +0200

    background added

--- js/script.js
+++ js/script.js
@@ -1,15 +1,27 @@
 {
-    console.log("Hello");
-const onChangeThemeclick = (backgroundButton) => {
+    const onChangeThemeclick = () => {
     const body = document.querySelector(".body");
+    const textColor = document.querySelector(".js-textColor");
+    const textColor2 = document.querySelector(".js-textColor2");
+    const selection = document.querySelector(".js-selection");
+    const selection2 = document.querySelector(".js-selection2");
+    const selection3 = document.querySelector(".js-selection3");
+    const selection4 = document.querySelector(".js-selection4");
+    const colorName = document.querySelector(".js-colorName");
 
     body.classList.toggle("body__backgroundDark");
+    textColor.classList.toggle("textColor__darkMode.textColor__darkMode:hover");
+    textColor2.classList.toggle("textColor__darkMode.textColor__darkMode:hover");
+    selection.classList.toggle("section__background");
+    selection2.classList.toggle("section__background");
+    selection3.classList.toggle("section__background");
+    selection4.classList.toggle("section__background");
 
-    backgroundButton.innerText = body.classList.contains(
+    colorName.innerText = body.classList.contains(
         "body__backgroundDark"
     )
-        ? "OFF"
-        : "ON";
+        ? "( OFF )"
+        : "( ON )";
 };
 
 const init = () => {
