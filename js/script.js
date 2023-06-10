39fd33b7944e56d1452788fe4fca5cfa0a45f036
Kacper Jarych <jarychkacper02@gmail.com>
2023-06-10 08:57:48 +0200

    correct button darkmode added

--- js/script.js
+++ js/script.js
@@ -1,34 +1,34 @@
 {
     const onChangeThemeclick = () => {
-    const body = document.querySelector(".body");
-    const textColor = document.querySelector(".js-textColor");
-    const textColor2 = document.querySelector(".js-textColor2");
-    const selection = document.querySelector(".js-selection");
-    const selection2 = document.querySelector(".js-selection2");
-    const selection3 = document.querySelector(".js-selection3");
-    const selection4 = document.querySelector(".js-selection4");
-    const colorName = document.querySelector(".js-colorName");
+        const body = document.querySelector(".body");
+        const textColor = document.querySelector(".js-textColor");
+        const textColor2 = document.querySelector(".js-textColor2");
+        const selection = document.querySelector(".js-selection");
+        const selection2 = document.querySelector(".js-selection2");
+        const selection3 = document.querySelector(".js-selection3");
+        const selection4 = document.querySelector(".js-selection4");
+        const colorName = document.querySelector(".js-colorName");
 
-    body.classList.toggle("body__backgroundDark");
-    textColor.classList.toggle("textColor__darkMode.textColor__darkMode:hover");
-    textColor2.classList.toggle("textColor__darkMode.textColor__darkMode:hover");
-    selection.classList.toggle("section__background");
-    selection2.classList.toggle("section__background");
-    selection3.classList.toggle("section__background");
-    selection4.classList.toggle("section__background");
+        body.classList.toggle("body__backgroundDark");
+        textColor.classList.toggle("textColor__darkMode");
+        textColor2.classList.toggle("textColor__darkMode");
+        selection.classList.toggle("section__background");
+        selection2.classList.toggle("section__background");
+        selection3.classList.toggle("section__background");
+        selection4.classList.toggle("section__background");
 
-    colorName.innerText = body.classList.contains(
-        "body__backgroundDark"
-    )
-        ? "( OFF )"
-        : "( ON )";
-};
+        colorName.innerText = body.classList.contains(
+            "body__backgroundDark"
+        )
+            ? "( OFF )"
+            : "( ON )";
+    };
 
-const init = () => {
-    const backgroundButton = document.querySelector(".js-backgroundButton");
-    backgroundButton.addEventListener("click", onChangeThemeclick);
+    const init = () => {
+        const backgroundButton = document.querySelector(".js-backgroundButton");
+        backgroundButton.addEventListener("click", onChangeThemeclick);
 
-};
+    };
 
     init();
 }
